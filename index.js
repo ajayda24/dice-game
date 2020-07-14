274969e6adb81a2fd009d40df2d86f232721f62c
Ajay Daniel Trevor <59312969+ajayda24@users.noreply.github.com>
2020-07-14 13:16:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
-var randomImage1 = "Images/dice" + randomNumber1 + ".png";
+var randomImage1 = "dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src",randomImage1);
 
 var randomNumber2 = Math.floor(Math.random()*6)+1;
-var randomImage2 = "Images/dice" + randomNumber2 + ".png";
+var randomImage2 = "dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
 
 if(randomNumber1 > randomNumber2){
